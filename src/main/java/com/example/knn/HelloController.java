74fa3fb91559e02843e17c730535a475f4dc8056
Kamil Witczak <222150@edu.p.lodz.pl>
2024-03-29 18:53:07 +0100

    Added id's to GUI

--- src/main/java/com/example/knn/HelloController.java
+++ src/main/java/com/example/knn/HelloController.java
@@ -2,6 +2,7 @@ package com.example.knn;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.HPos;
@@ -15,22 +16,48 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
     @FXML
     private ChoiceBox<String> chooseMetric;
-
-    @FXML
-    private TableView<String> table1;
-    @FXML
-    private TableColumn<String, String> t11;
     @FXML
     private GridPane grid2;
-
     @FXML
     private GridPane grid1;
+    @FXML
+    private TextField articlesNumber;
+    @FXML
+    private TextField neighbors;
+    @FXML
+    private TextField percentage;
+    @FXML
+    private CheckBox c1;
+    @FXML
+    private CheckBox c2;
+    @FXML
+    private CheckBox c3;
+    @FXML
+    private CheckBox c4;
+    @FXML
+    private CheckBox c5;
+    @FXML
+    private CheckBox c6;
+    @FXML
+    private CheckBox c7;
+    @FXML
+    private CheckBox c8;
+    @FXML
+    private CheckBox c9;
+    @FXML
+    private CheckBox c10;
+    @FXML
+    private CheckBox c11;
+    @FXML
+    private Button button;
+
     private ObservableList<String> availableMetrics = FXCollections.observableArrayList("Euklidesowa", "Miejska", "Czebyszewa");
     private ObservableList<String> rows1 = FXCollections.observableArrayList("USA", "UK", "CANADA", "GERMANY", "FRANCE", "JAPAN");
     private ObservableList<String> rows2 = FXCollections.observableArrayList("Precision", "Recall", "F1");
@@ -59,5 +86,26 @@ public class HelloController implements Initializable {
             GridPane.setHalignment(text, HPos.CENTER);
             grid2.add(text, 0, i);
         }
+    }
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("Przycisk został naciśnięty");
+    }
+
+    public String getChooseMetric() {
+        return chooseMetric.getValue();
+    }
+
+    public String  getArticlesNumber() {
+        return articlesNumber.getText();
+    }
+
+    public Integer getNeighbors() {
+        return Integer.parseInt(neighbors.getText());
+    }
+
+    public Integer getPercentage() {
+        return Integer.parseInt(percentage.getText());
     }
 }
